79c0988306e256d20160783596ec1b2fb9f78ed5
Tushar <tusharsharma2509@gmail.com>
2023-04-24 22:42:44 +0530

    Initial Setup for visual studio

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DSA_CCI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Strings/UniqueCharacters.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace DSA_CCI.Strings;
+
+internal class UniqueCharacters
+{
+    // problem 1.1 in CCI
+    public static bool isUniqueChars(string input)
+    {
+        bool result = true;
+        // make a hash table
+        Hashtable hashtable = new Hashtable();
+        for(int i=0; i<input.Length; i++)
+        {
+            if (hashtable[input[i]] == null)
+            {
+                hashtable.Add(input[i], i);
+                continue;
+            }
+            else
+            {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+}
